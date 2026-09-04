f2e8aa3bf3a1132b6897aa8777dbee5dfb0607f8
jovanMatos <jovana.matos@challenge.pt>
2026-09-04 19:47:13 +0100

    API products controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+using System.Linq;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductsController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_repository.GetAll());
+        }
+    }
+}
